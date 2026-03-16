e3c1e7adfe50b323638fd6321cab8fbccce65bff
Davi-Teodoro <davitrynity@gmail.com>
2026-03-16 20:58:58 -0300

    Aula 2a Variaveis e desafios

---
+++ Desafio/Desafio Aula02a.py
@@ -0,0 +1,14 @@
+# desafio 1
+nome = input("Digite seu nome: ")
+print(f"Bem-vindo! {nome}")
+
+#desafio 2
+dia = int(input("digite o dia de seu nascimento "))
+mes = int(input("Digite o numero do mes de seu nascimento "))
+ano = int(input("Digite o ano de seu nascimento "))
+print(f"Você nasceu no dia {dia}, no mês {mes} e no ano {ano}")
+
+#desafio 3
+num1 = float(input("digite um numero "))
+num2 = float(input("digite outro numero "))
+print(f"a soma dos dois numeros é: {num1 + num2}")
