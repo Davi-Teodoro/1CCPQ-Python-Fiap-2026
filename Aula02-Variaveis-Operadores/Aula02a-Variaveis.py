a7a4e1896dd2f4187f9b076b140a2a185898c8f6
Davi-Teodoro <davitrynity@gmail.com>
2026-03-16 20:50:09 -0300

    Aula 2a Variaveis

---
+++ Aula02-Variaveis-Operadores/Aula02a-Variaveis.py
@@ -0,0 +1,24 @@
+print("Olá,mundo! ")
+
+print(7 + 4)
+print('7' + '4') # Concatenando strings
+
+'''
+Comentário de multiplas linhas
+'''
+
+# Variaveis
+
+nome = "davi"
+idade = 19
+peso= 896098
+
+print("Olá",nome, idade, peso)
+print(f"Olá, {nome}!")
+
+# INPUTS --- SIMULAÇÃO DE UM FORMS NO CMD
+
+nome = str(input("Digite seu nome, porra!: "))
+idade = int(input("Digite sua idade, porra!: "))
+peso = int(input("Digite seu peso, porra!: "))
+print(nome, idade, peso)
